12d61ed9d3faeb66cb17caae7c1f5a26a70be13b
Cheng Yu <yucheng618@gmail.com>
2022-02-01 18:38:52 -0500

    first commit

---
+++ likes/script.js
@@ -0,0 +1,20 @@
+var neillikes = document.querySelector("#neillikes");
+var numofneil = 9;
+function neiladd() {
+    numofneil ++;
+    neillikes.innerText = numofneil + " like(s)"
+}
+
+var nicholelikes = document.querySelector("#nicholelikes");
+var numofnichole = 12;
+function nicholeadd() {
+    numofnichole ++;
+    nicholelikes.innerText = numofnichole + " like(s)"
+}
+
+var jimlikes = document.querySelector("#jimlikes");
+var numofjim = 9;
+function jimadd() {
+    numofjim ++;
+    jimlikes.innerText = numofjim + " like(s)"
+}
