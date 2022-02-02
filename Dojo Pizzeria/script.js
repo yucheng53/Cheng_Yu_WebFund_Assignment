ad5e3a9013d6e0ba77fa1089d600cfa6ce18a279
Cheng Yu <yucheng618@gmail.com>
2022-02-02 10:47:31 -0500

    first commit

---
+++ Dojo Pizzeria/script.js
@@ -0,0 +1,85 @@
+function pizzaOven(crustType, sauceType, cheeses, toppings) {
+    var pizza = {};
+    pizza.crustType = crustType;
+    pizza.sauceType = sauceType;
+    pizza.cheeses = cheeses;
+    pizza.toppings = toppings;
+    return pizza;
+}
+    
+var p1 = pizzaOven("deep dish", "traditional", ["mozzarella"], ["pepperoni", "sausage"]);
+var p2 = pizzaOven("hand tossed", "marinara", ["mozzarella", "feta"], ["mushrooms", "olives", "onions"])
+var p3 = pizzaOven("thin", "hot", "cheddar", ["beaf", "onion"])
+var p4 = pizzaOven("thick", "tartar","gouda", ["chicken", "pineapple"])
+
+var crustTypes = [
+    "deep dish",
+    "hand tossed",
+    "thin and crispy",
+    "cauliflower",
+    "gluten free",
+    "hawaiian bread"
+];
+
+var sauceTypes = [
+    "traditional",
+    "marinara",
+    "bbq",
+    "white sauce",
+    "nacho cheese",
+    "tikka masala"
+];
+
+var cheeses = [
+    "mozzarella",
+    "cheddar",
+    "feta",
+    "swiss cheese",
+    "blue cheese",
+    "goat cheese",
+    "provolone",
+    "parmesan",
+    "vegan cheese"
+];
+
+var toppings = [
+    "pepperoni",
+    "sausage",
+    "chicken",
+    "corn",
+    "olives",
+    "bell peppers",
+    "onions",
+    "mushrooms",
+    "anchovies"
+];
+
+function randomRange(max, min) {
+    return Math.floor(Math.random() * max - min) + min;
+}
+
+function randomPick(arr) {
+    var i = Math.floor(arr.length * Math.random());
+    return arr[i];
+}
+
+function randomPizza() {
+    var pizza = {};
+    pizza.crustType = randomPick(crustTypes);
+    pizza.sauceType = randomPick(sauceTypes);
+    pizza.cheeses = [];
+    pizza.toppings = [];
+    for(var i=0; i<randomRange(5, 1); i++) {
+        pizza.cheeses.push(randomPick(cheeses));
+    }
+    for(var i=0; i<randomRange(4, 0); i++) {
+        pizza.toppings.push(randomPick(toppings));
+    }
+    return pizza;
+}
+
+console.log(randomPizza());
+
+
+
+
