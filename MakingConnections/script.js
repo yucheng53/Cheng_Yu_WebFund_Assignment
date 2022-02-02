6972405705e85693596d288300abfe23d70422ac
Cheng Yu <yucheng618@gmail.com>
2022-02-01 22:15:27 -0500

    bonus add

--- MakingConnections/script.js
+++ MakingConnections/script.js
@@ -4,11 +4,24 @@ function changename() {
 }
 
 var userline1 = document.querySelector(".user-line1");
+var connections1 = document.querySelector("#connections1");
+var connection1num = 2;
 function nameremove1() {
     userline1.remove();
+    connection1num --;
+    connections1.innerText = connection1num;
 }
 
 var userline2 = document.querySelector(".user-line2");
 function nameremove2() {
     userline2.remove();
+    connection1num --;
+    connections1.innerText = connection1num;
+}
+
+var connections2 = document.querySelector("#connections2");
+var connection2num = 500;
+function nameadd() {
+    connection2num ++;
+    connections2.innerText = connection2num;
 }
