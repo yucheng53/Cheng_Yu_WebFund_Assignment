919bf7cd2ab8f3a35c90ca292783a51468c7c130
Cheng Yu <yucheng618@gmail.com>
2022-02-01 21:52:34 -0500

    first commit

---
+++ MakingConnections/script.js
@@ -0,0 +1,14 @@
+var janesname = document.querySelector("#janesname");
+function changename() {
+    janesname.innerText = "Todde E";
+}
+
+var userline1 = document.querySelector(".user-line1");
+function nameremove1() {
+    userline1.remove();
+}
+
+var userline2 = document.querySelector(".user-line2");
+function nameremove2() {
+    userline2.remove();
+}
